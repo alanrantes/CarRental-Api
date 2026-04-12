7bc16a7bfb94735c041dcb4bad0805add4618c03
alanrantes <alan.arantes.dev@gmail.com>
2026-04-12 11:40:42 -0300

    feat: adicionando classe Fabricante

---
+++ CarRental-Api/Models/Fabricante.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental_Api.Models
+{
+    public class Fabricante
+    {
+        [Key]
+        public int IdFabricante { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        public string? PaisOrigem { get; set; }
+
+    }
+}
