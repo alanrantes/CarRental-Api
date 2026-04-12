a33a2d898192f3eef7e89e723ae209bc1ef9a703
alanrantes <alan.arantes.dev@gmail.com>
2026-04-12 11:47:59 -0300

    feat: iniciando configurações AppDbContext

---
+++ CarRental-Api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarRental_Api.Models;
+
+namespace CarRental_Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<CategoriaVeiculo> CategoriasVeiculo { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Aluguel> Alugueis { get; set; }
+    }
+}
