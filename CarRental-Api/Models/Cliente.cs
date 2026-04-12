6356491225b15c9cb227cd6787dde317d5971957
alanrantes <alan.arantes.dev@gmail.com>
2026-04-12 11:36:04 -0300

    feat: estrutura inicial do projeto com a classe Cliente

---
+++ CarRental-Api/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental_Api.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Cpf { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Telefone { get; set; }
+    }
+}
