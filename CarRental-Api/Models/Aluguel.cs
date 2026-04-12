f5df55c64585e9d59b39c5783a1030d0c92d8977
alanrantes <alan.arantes.dev@gmail.com>
2026-04-12 11:45:01 -0300

    feat: adicionando classe Aluguel

---
+++ CarRental-Api/Models/Aluguel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRental_Api.Models
+{
+    public class Aluguel
+    {
+        [Key]
+        public int IdAluguel { get; set; }
+
+        [Required]
+        public DateTime DataRetirada { get; set; }
+
+        [Required]
+        public DateTime DataPrevistaDevolucao { get; set; }
+
+        public DateTime? DataDevolucao { get; set; }
+
+        [Required]
+        public int QuilometragemInicial { get; set; }
+
+        public int? QuilometragemFinal { get; set; }
+
+        [Required]
+        public decimal ValorDiaria { get; set; }
+
+        [Required]
+        public decimal ValorTotal { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        public int IdCliente { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Cliente Cliente { get; set; }
+
+        public int IdVeiculo { get; set; }
+
+        [ForeignKey("IdVeiculo")]
+        public Veiculo Veiculo { get; set; }
+    }
+}
