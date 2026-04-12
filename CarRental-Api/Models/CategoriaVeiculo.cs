0fec28b38fc594bb50393cbfe45deb7a85dc8122
alanrantes <alan.arantes.dev@gmail.com>
2026-04-12 11:42:13 -0300

    feat: adicionando classe CategoriaVeiculo

---
+++ CarRental-Api/Models/CategoriaVeiculo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CarRental_Api.Models
+{
+    public class CategoriaVeiculo
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public decimal ValorDiariaBase { get; set; }
+    }
+}
